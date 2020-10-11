9376ad4ebcc067961fbb7b4291e289c11a728539
AntiCakjesCult <72666992+AntiCakjesCult@users.noreply.github.com>
2020-10-11 11:44:17 +0200

    Update Pokemon.py

--- Pokemon.py
+++ Pokemon.py
@@ -13,19 +13,21 @@ def story1():
     return storyline1
 
 def story2():
-    storyline2 = print(
+    storyline2 = int(input(
         'Aah je hebt level 1 gehaald!!'
-        'Je pokemon is sterker geworden'
-        'Je vecht nu tegen een pokemon uit level 2'
-        )
+        'Kies een van de volgende pokemon voor ronde level 2'
+        'Je vecht nu tegen een andere pokemon uit level 2'
+        'Je hebt keuze uit: Rillaboom[1], Inteleon[2], Cinderace[3], Grimmsnarl[4],'
+        ' Corviknight[5], Obstagoon[6], Perrserker[7]'
+        ))-1
 
 def story3():
     storyline3 = print(
         'AAAAH goed gedaan je hebt ook level 2 behaald!!!'
         'Nu de ben je bij het laatste level gekomen'
-        'Het schijnt dat hier de moeilijkste en de meest ontvindbare pokemon zich koest houden.')
+        'Het schijnt dat hier de moeilijkste en de meest ontvindbare pokemon zich koest houden en jou uitdagen.')
 
-def pokemon_level1():
+def level1():
     Scorebunny = ['Scorebunny', 18, 2, 5]
     Sobble = ['Sobble',17, 3, 6]
     Grookey = ['Grookey',18, 2, 6]
@@ -35,14 +37,12 @@ def pokemon_level1():
     Blibbug = ['Blibbug', 16, 3, 5]
     pokemons = [Scorebunny, Sobble, Grookey, Rookidee,
                 Greedent, Wooloo, Blibbug]
-    random_pokemon1 = random.randint(1,7)-1
-    return pokemons, random_pokemon1
+    random1 = random.randint(1,7)-1
+    return pokemons, random1
 
-def evolve(e_chosen_pokemon1, e_pokemonlist1,e_pokemonlist2):
-    if e_chosen_pokemon1 == Scorebunny:
         
 
-def pokemon_level2():
+def level2():
     Rillaboom = ['Rillaboom',180, 30, 50]
     Inteleon = ['Inteleon',190, 25, 60]
     Cinderace = ['Cinderace',195, 20, 70]
@@ -52,10 +52,10 @@ def pokemon_level2():
     Perrserker = ['Perrserker', 170, 50, 65]
     pokemons = [Rillaboom, Inteleon, Cinderace, Grimmsnarl,
                 Corviknight, Obstagoon, Perrserker]
-    random_pokemon3 = random.randint(1,8)-1
-    return pokemons, random_pokemon2
+    random2 = random.randint(1,7)-1
+    return pokemons, random2
 
-def pokemon_level3():
+def level3():
     Zacian = ['Zacian', 240, 50, 90]
     Zamazenta = ['Zamazenta', 270, 40, 80]
     Eternatus = ['Eternatus', 255, 60, 95]
@@ -64,11 +64,10 @@ def pokemon_level3():
     Calyrax = Urshifu_st = ['Calyrax', 260, 55, 85]
     pokemons = [Zacian, Zamazenta, Eternatus, Urshifu_st,
                 Urshifu_rt, Calyrax]
-    random_pokemon3 = random.randint(1,6)-1
-    return pokemons, random_pokemon3
+    random3 = random.randint(1,6)-1
+    return pokemons, random3
     
 def fight_display(p_chosen, p_random):
-
     print('Je gekozen pokemon is:  ' + str(p_chosen[0]) + '!' + '\nZijn HP is ' + str(p_chosen[1]) +
           ', zijn aanval is tussen de ' + str(p_chosen[2]) + ' en ' + str(p_chosen[3]) + '!!')
     print('\nJe willekeurige gekozen tegenstander is:  ' + str(p_random[0]) + '!' + '\nZijn HP is ' + str(p_random[1]) +
@@ -82,22 +81,34 @@ def fight(f_chosen, f_random):
         attack_f_random = random.randint(f_random[2], f_random[3])
         pkm1 -= attack_f_random
         pkm2 -= attack_f_chosen
-        print('De HP van ' + f_chosen[0] + ' is nu ' + str(pkm1))
         print('Het HP van ' + f_random[0] + ' is nu ' + str(pkm2))
+        print('De HP van ' + f_chosen[0] + ' is nu ' + str(pkm1))
     return pkm1, pkm2
 
 def main(): 
     user_input = story1()
-    list_pokemon1, randomlvl1 = pokemon_level1()
-    chosen_pokemon1 = list_pokemon1[user_input]
-    random_chosen_pokemon1 = list_pokemon1[randomlvl1]
-    fight_display(chosen_pokemon1, random_chosen_pokemon1)
-    pkm1, pkm2 = fight(chosen_pokemon1, random_chosen_pokemon1)
+    list1, randomlvl1 = level1()
+    chosen1 = list1[user_input]
+    random_chosen1 = list1[randomlvl1]
+    fight_display(chosen1, random_chosen1)
+    pkm1, pkm2 = fight(chosen1, random_chosen1)
+    pkm3 = 0
+    pkm4 = 0
     if pkm1 > pkm2:
-        pokemon_level()
-        list_pokemon2, randomlvl2 = pokemon_level1()
-        evolve(chosen_pokemon1,list_pokemon1,list_pokemon2)
-        
-        
-    list_pokemon3, randomlvl3 = pokemon_level1()
+        list2, randomlvl2 = level2()
+        user_input2 = story2()
+        chosen2 = list2[user_input2]
+        random_chosen2 = list2[randomlvl2]
+        fight_display(chosen2, random_chosen2)
+        pkm3, pkm4 = fight(chosen2, random_chosen2)  
+    elif pkm3 > pkm4:
+        list2, randomlvl2 = level2()
+        story3()
+        chosen2 = list2[user_input2]
+        random_chosen3 = list3[randomlvl3]
+        fight_display(chosen2, random_chosen3)
+        fight(chosen2, random_chosen3)  
+    else:
+        print('Je hebt verloren')
+    #print('Gefeliciteerd, je hebt gewonnen met'+ chosen2)
 main()
